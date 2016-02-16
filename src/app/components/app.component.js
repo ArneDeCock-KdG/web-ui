e4d190c13f0b512c8abb4d801b3f5dda94bd03e7
Arne De Cock <arne.decock.1@student.kdg.be>
2016-02-16 16:32:35 +0100

    optimazitation

--- src/app/components/app.component.js
+++ src/app/components/app.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core', "./authentication/welcome.component"], function(exports_1) {
+System.register(['angular2/core', "./toolbar.component", "./authentication/register.component"], function(exports_1) {
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
         var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
         if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -8,15 +8,18 @@ System.register(['angular2/core', "./authentication/welcome.component"], functio
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, welcome_component_1;
+    var core_1, toolbar_component_1, register_component_1;
     var AppComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
             },
-            function (welcome_component_1_1) {
-                welcome_component_1 = welcome_component_1_1;
+            function (toolbar_component_1_1) {
+                toolbar_component_1 = toolbar_component_1_1;
+            },
+            function (register_component_1_1) {
+                register_component_1 = register_component_1_1;
             }],
         execute: function() {
             AppComponent = (function () {
@@ -25,8 +28,8 @@ System.register(['angular2/core', "./authentication/welcome.component"], functio
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'my-app',
-                        template: "\n        <div id=\"main-content\">\n        <section id=\"register\">\n            <welcome></welcome>\n        </section>\n        </div>\n\n        <footer></footer>\n    ",
-                        directives: [welcome_component_1.WelcomeComponent]
+                        template: "\n        <toolbar></toolbar>\n\n        <div id=\"main-content\">\n        <section id=\"register\">\n            <register></register>\n        </section>\n        </div>\n\n        <footer>\n            <p>test <a>link</a></p>\n        </footer>\n    ",
+                        directives: [toolbar_component_1.ToolbarComponent, register_component_1.RegisterComponent]
                     }), 
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
--- src/app/components/app.component.ts
+++ src/app/components/app.component.ts
@@ -1,18 +1,24 @@
 import {Component} from 'angular2/core';
 import {WelcomeComponent} from "./authentication/welcome.component";
+import {ToolbarComponent} from "./toolbar.component";
+import {RegisterComponent} from "./authentication/register.component";
 
 @Component({
     selector: 'my-app',
     template: `
+        <toolbar></toolbar>
+
         <div id="main-content">
         <section id="register">
-            <welcome></welcome>
+            <register></register>
         </section>
         </div>
 
-        <footer></footer>
+        <footer>
+            <p>test <a>link</a></p>
+        </footer>
     `,
-    directives: [WelcomeComponent]
+    directives: [ToolbarComponent, RegisterComponent]
 })
 export class AppComponent {
 }
